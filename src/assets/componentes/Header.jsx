36b75ebf2c6eeb5fd89ca8b50dbb85538da696f0
Alex-Maldonado <alexandermaldonadosoto@gmail.com>
2024-03-23 20:28:39 -0600

    style(>Header): :art: Colocando la version de desktop del header, Hay un problema con el margin y el centrado del header

--- src/assets/componentes/Header.jsx
+++ src/assets/componentes/Header.jsx
@@ -1,60 +1,95 @@
-import { Container, Typography } from "@mui/material";
-import GitHubIcon from '@mui/icons-material/GitHub';
+import { Box, Container, Typography } from "@mui/material";
+import Image from "../Images/img";
+import GitHubIcon from "@mui/icons-material/GitHub";
 import React from "react";
 
 export default function Header() {
+
+//////Hya un bug con el margin fue lo que le quite al header, se traba y no funciona
+/////Hay que centrarlo con flex box para que se puedan colocar las imagenes tambien
+
   return (
     <Container
-        component={"header"}
-        sx={{
-          display: "grid",
-          padding: "10% 0",
-          placeItems: "center",
-          height: "100vh",
-          alignContent: "center",
-          gap: "1rem",
-        }}
+      component={"header"}
+      sx={{
+        display: "flex",
+        padding: "10% 0",
+        placeItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+        alignContent: "center",
+        margin: "0",
+      }}
     >
-      <Typography
-        variant="h4"
-        component="span"
-        display={"block"}
-        align="center"
-        fontWeight={"bold"}
-        color={"text.secondary"}
+      <Box
+        component={"hgroup"}
+        sx={{
+          width: { xs: "100%", md: "100%" },
+          textAlign: { xs: "center", md: "left" },
+          justifyItems: { xs: "center", md: "left" },
+        }}
+        display={"grid"}
       >
-        Hola
         <Typography
-          variant="h4"
           component="span"
-          display={"inline"}
-          color={"primary.main"}
-        >
-          .
-        </Typography>
-      </Typography>
-      <Typography
-        variant="h4"
-        component="hgroup"
-        align="center"
-        fontWeight={"bold"}
-        color={"primary.main"}
-      >
-        {"</"}
-        <Typography
-          variant="h4"
-          component="span"
-          align="center"
+          display={"block"}
+          fontWeight={"bold"}
           color={"text.secondary"}
+          fontSize={{ xs: "2rem", md: "3rem" }}
         >
-          {"Soy "}
+          Hola
+          <Typography
+            component="span"
+            display={"inline"}
+            color={"primary.main"}
+            fontSize={{ xs: "2rem", md: "3rem" }}
+          >
+            .
+          </Typography>
         </Typography>
-        {"Alex Maldonado>"}
-      </Typography>
-      <Typography variant="h5" component="h1" align="center" color={"text.secondary"}>
-        Desarrollador web en Guatemala
-      </Typography>
-        <GitHubIcon sx={{ color: "text.secondary", fontSize: "2rem" }} />
+        <Typography
+          component="hgroup"
+          fontWeight={"bold"}
+          color={"primary.main"}
+          fontSize={{ xs: "2rem", md: "2.5rem" }}
+        >
+          {"</"}
+          <Typography
+            component="span"
+            color={"text.secondary"}
+            fontSize={{ xs: "2rem", md: "2.5rem" }}
+          >
+            {"Soy "}
+          </Typography>
+          {"Alex Maldonado>"}
+        </Typography>
+        <Typography component="h1" color={"text.secondary"}  fontSize={{ xs: "1.5rem", md: "1.5rem" }}>
+          Desarrollador web en Guatemala
+        </Typography>
+        <GitHubIcon sx={{ color: "text.secondary", paddingTop: "10px" ,fontSize: { xs: "2rem", md: "3rem" } }} />
+      </Box>
+
+      <Box
+        component={"picture"}
+        sx={{
+          width: { md: "100%" },
+          height: "auto",
+          display: { xs: "none", md: "block" },
+        }}
+
+      >
+        <img
+          src={Image.imgMain}
+          alt=""
+          style={{
+            width: "100%",
+            height: "auto",
+            objectFit: "cover",
+            objectPosition: "center",
+          }}
+        />
+      </Box>
+
     </Container>
   );
 }
